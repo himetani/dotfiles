2668dc6302cbe73a583f6801bdc5e4c56009097c
himetani <h.s.g.616top@gmail.com>
2021-03-15 19:21:59 +0900

    add key mapping for git

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,5 +11,17 @@ vim.api.nvim_set_var("cursorhold_updatetime", 100)
 vim.api.nvim_set_var("startify_bookmarks", {"~/.config/nvim/init.vim"})
 
 -- telescope.vim
-vim.api.nvim_set_keymap('n', '<leader>ff','<cmd>Telescope find_files<cr>', { silent=true, noremap=true })
-vim.api.nvim_set_keymap('n', '<leader>fg','<cmd>Telescope live_grep<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>ll','<cmd>Telescope find_files<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>f','<cmd>Telescope live_grep<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gst','<cmd>Telescope git_status<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gbranch','<cmd>Telescope git_branches<cr>', { silent=true, noremap=true })
+
+-- vim-fugitive
+vim.api.nvim_set_keymap('n', 'gdiff','<cmd>Gdiffsplit<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gadd','<cmd>Gwrite<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gcommit','<cmd>Gcommit<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gblame','<cmd>Gblame<cr>', { silent=true, noremap=true })
+vim.api.nvim_set_keymap('n', 'gpull','<cmd>Gpull<cr>', { silent=true, noremap=true })
+
+-- tagbar
+vim.api.nvim_set_keymap('n', '<c-g>','<cmd>Tagbar<cr>', { silent=true, noremap=true })
